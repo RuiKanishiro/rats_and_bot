6b2a79391d5fe41bf85033a4345b03b72f25ffc2
Nadya Sharova <150694738+RuiKanishiro@users.noreply.github.com>
2025-05-15 05:15:29 +0500

    переводчик

---
+++ name_translator.py
@@ -0,0 +1,10 @@
+from library import *
+
+
+def translation(city: str):
+    try:
+        translator = Translator(from_lang='russian', to_lang='english')
+        result = translator.translate(city)
+        return result
+    except RuntimeError as e:
+        return f'Ошибка {e}'
